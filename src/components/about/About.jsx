9724371731358bfa7ab48597cbb3ee4a35696ccf
DanielCanada <s1820987@usls.edu.ph>
2022-12-12 13:15:52 +0800

    v2 fix info update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -29,7 +29,7 @@ const About = () => {
         <div className="about__content">
         <h2>About Me</h2>
           <p>
-            {'\t'}I am Daniel from Bacolod City. A Computer Engineering fresh graduate at the University of St. La Salle - Bacolod. I became External Vice President of ICpEP.se USLS from mid 2019 to May 2021 and made Dean's List in the AY 2020 - 2021. Interested in Software and Application Development, UI/UX Design, and Networking. A solution-oriented and adaptive individual committed to service and team coordination to achieve deliverables.
+            {'\t'}I am Daniel from Bacolod City. A Computer Engineering fresh graduate at the University of St. La Salle - Bacolod. I became External Vice President of ICpEP.se USLS from mid 2019 to May 2021 and made Dean's List in the AY 2020 - 2021 and AY 2021 - 2022. Interested in Software and Application Development, UI/UX Design, and Networking. A solution-oriented and adaptive individual committed to service and team coordination to achieve deliverables.
           </p>
 
           <div className="about__cards">
